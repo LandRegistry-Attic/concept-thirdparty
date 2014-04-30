8594ed6872b788a889aa33f41fd534fd007bc40c
chrisheaththomas <chrisheaththomas@hotmail.com>
2014-04-30 11:04:20 +0000

    converted to web app

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+	return render_template('index.html')
+
+@app.route('/authorise')
+def authorise():
+	return render_template('authorise.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8006, debug=True)    
